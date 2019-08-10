661b859c2332118d05b7a4e375454a02465870c7
Kyanij Maharjan <mkyanij1@gmail.com>
2019-08-10 13:40:28 +0545

    Updates

---
+++ precache-manifest.d95b9791a03f3fcd3029133aae66225e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8386bb410f18f75986391a8cd34d56ff",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "76bb346d468635a2da0b",
+    "url": "/recipe-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "717fe0f7efa8adb4d33a",
+    "url": "/recipe-app/static/css/main.eed54a9a.chunk.css"
+  },
+  {
+    "revision": "76bb346d468635a2da0b",
+    "url": "/recipe-app/static/js/2.1de889bc.chunk.js"
+  },
+  {
+    "revision": "717fe0f7efa8adb4d33a",
+    "url": "/recipe-app/static/js/main.9b0ed64e.chunk.js"
+  },
+  {
+    "revision": "82c36fb6d19137cb8b75",
+    "url": "/recipe-app/static/js/runtime~main.11ea1cab.js"
+  }
+]);
